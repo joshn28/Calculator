c82bb433ac7ed6bf17d67df0694ce586891bbbc0
joshn28 <jono2td8@gmail.com>
2021-12-30 12:19:34 -0500

    Created operate function

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 function add(a, b) {
     return a + b;
 }
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(op, a, b) {
+    let result;
+    switch (op) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}

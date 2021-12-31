3cddc8a23cf6c6a7421562b78c08f61ac06bf0e0
joshn28 <jono2td8@gmail.com>
2021-12-31 14:20:03 -0500

    Made functions to populate display when clicked and made calculator work

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+
+let display = "";
+let firstNumber;
+let operator;
+
+const numberButtons = document.querySelectorAll('.numbers');
+const opButtons = document.querySelectorAll('.operator');
+const screen = document.querySelector('#screen');
+const equalButton = document.querySelector('#equals');
+
+equalButton.addEventListener('click', (button) => {
+    display = operate(operator, firstNumber, parseFloat(display));
+    firstNumber = display;
+    operator = button.textContent;
+    screen.textContent = display;
+    display = "";
+});
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        display += button.textContent;
+        screen.textContent = display; 
+    });
+});
+
+opButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (typeof operator === 'undefined') {
+            firstNumber = parseFloat(display);
+            operator = button.textContent;
+        } else if (display == "") {
+            operator = button.textContent;
+        } else {
+            display = operate(operator, firstNumber, parseFloat(display));
+            firstNumber = display;
+            operator = button.textContent;
+            screen.textContent = display;
+        }
+        display = "";
+    });
+});
+
 function add(a, b) {
     return a + b;
 }
